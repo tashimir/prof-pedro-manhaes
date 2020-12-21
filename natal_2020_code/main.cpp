fca5be4ed2ab88d3cc0c14f772a5ea98bfa8391c
Pedro Machado Manhães de Castro <66933127+tashimir@users.noreply.github.com>
2020-12-21 20:18:33 -0300

    Update main.cpp

--- natal_2020_code/main.cpp
+++ natal_2020_code/main.cpp
@@ -40,36 +40,23 @@
 *              *************************
 */
 
-// imprime um * 
-// (verde, se voce estiver no windows 10)
-inline void verde()
-{ 
-    // primeira linha e terceira linha
-    // estao ai para colorir de verde
-    // se voce estiver no Windows10 deve funcionar
-    // senao, voce pode comentar essas linhas mesmo
-    std::cout << "\033[32m"; 
-    std::cout << "*";
-    std::cout << "\033[0m";
-}
-
 int main()
 {
-    const int n = 5;
-    // sua arvore de natal vai ter altura n^2
-    // e vai ter uma base de tamanho 4n-3
-    int base = 6 * n - 5;
+    const int N = 5;
+    // sua arvore de natal vai ter altura (3n-1)n/2
+    // e vai ter uma base de tamanho 6n-5
+    const int base = 6 * N - 5;
     int nb_estrelas_na_l1_da_estrutura = 1;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
         int nb_estrelas_na_l_atual = nb_estrelas_na_l1_da_estrutura;
-        for (int j = 0; j < n+i; j++)
+        for (int j = 0; j < N+i; j++)
         {
             int nb_espacos = (base - nb_estrelas_na_l_atual) / 2;
             for (int k = 0; k < nb_espacos; k++)
                 std::cout << " ";
             for (int k = 0; k < nb_estrelas_na_l_atual; k++)
-                verde();
+                std::cout << "\033[32m*\033[0m";
             std::cout << std::endl;
             nb_estrelas_na_l_atual += 2;
         }
