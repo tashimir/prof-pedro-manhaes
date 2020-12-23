6478db5e17e286bfb83767c1bcf28cf5cf72484d
Pedro Machado Manhães de Castro <66933127+tashimir@users.noreply.github.com>
2020-12-23 06:31:29 -0300

    Update main.cpp

--- natal_2020_code/main.cpp
+++ natal_2020_code/main.cpp
@@ -47,6 +47,7 @@ int main()
     // e vai ter uma base de tamanho 6n-5
     const int base = 6 * N - 5;
     int nb_estrelas_na_l1_da_estrutura = 1;
+    std::cout << "\033[32m";
     for (int i = 0; i < N; i++)
     {
         int nb_estrelas_na_l_atual = nb_estrelas_na_l1_da_estrutura;
@@ -56,13 +57,13 @@ int main()
             for (int k = 0; k < nb_espacos; k++)
                 std::cout << " ";
             for (int k = 0; k < nb_estrelas_na_l_atual; k++)
-                std::cout << "\033[32m*\033[0m";
+                std::cout << "*";
             std::cout << std::endl;
             nb_estrelas_na_l_atual += 2;
         }
         nb_estrelas_na_l1_da_estrutura += 2;
     }
-
+    std::cout << "\033[0m";
     std::cout << std::endl;
     return 0;
 }
